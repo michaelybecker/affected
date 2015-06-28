26a632c4b9b77d5873db8266a0a2e74b1f37b0fd
Michael Hazani <123mickey@gmail.com>
2015-06-27 17:23:49 -0700

    pre-convolver change commit

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -13,12 +13,21 @@ var playing = false;
 
 
 var url = "audio/piano_sample.ogg";
+var convolver = audioCtx.createConvolver();
 
 
-function getStream(stream) {
-  var mediaStreamSource = audioCtx.createMediaStreamSource(stream);
-  mediaStreamSource.connect(audioCtx.destination);
-}
+// function getStream(stream) {
+//   var mediaStreamSource = audioCtx.createMediaStreamSource(stream);
+//   mediaStreamSource.connect(audioCtx.destination);
+// }
+loadSound(url);
+
+// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+// navigator.getUserMedia({audio: true}, getStream, error);
+
+// function error() {
+//   alert('stream generation failed.');
+// }
 
 function loadSound(url) {
   var request = new XMLHttpRequest();
@@ -28,7 +37,10 @@ function loadSound(url) {
 
     audioCtx.decodeAudioData(request.response, function(buffer) {
       buf = buffer;
-      //playSound();
+      source = audioCtx.createBufferSource();
+      source.buffer = buf;
+      source.connect(convolver);
+      convolver.connect(audioCtx.destination);
     });
   };
 
@@ -37,21 +49,16 @@ function loadSound(url) {
 }
 
 function playSound(buffer) {
-  source = audioCtx.createBufferSource();
-  source.buffer = buf;
-  source.connect(audioCtx.destination);
-  source.start(0);
+
+convolver.buffer = source.buffer;
+
+source.start(0);
+  // source.connect(audioCtx.destination);
+  // source.start(0);
 
 }
 
-loadSound(url);
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-navigator.getUserMedia({audio: true}, getStream, error);
-
-function error() {
-  alert('stream generation failed.');
-}
 
 
 $(document).keypress(function(e) {
@@ -76,36 +83,10 @@ console.log(e.charCode);
 
 });
 
-//effect -- moog filter
+//effect -- convolver
 
-var bufferSize = 4096;
-var effect = (function() {
-    var node = audioCtx.createScriptProcessor(bufferSize, 1, 1);
-    var in1, in2, in3, in4, out1, out2, out3, out4;
-    in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0.0;
-    node.cutoff = 0.065; // between 0.0 and 1.0
-    node.resonance = 3.99; // between 0.0 and 4.0
-    node.onaudioprocess = function(e) {
-        var input = e.inputBuffer.getChannelData(0);
-        var output = e.outputBuffer.getChannelData(0);
-        var f = node.cutoff * 1.16;
-        var fb = node.resonance * (1.0 - 0.15 * f * f);
-        for (var i = 0; i < bufferSize; i++) {
-            input[i] -= out4 * fb;
-            input[i] *= 0.35013 * (f*f)*(f*f);
-            out1 = input[i] + 0.3 * in1 + (1 - f) * out1; // Pole 1
-            in1 = input[i];
-            out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
-            in2 = out1;
-            out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
-            in3 = out2;
-            out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
-            in4 = out3;
-            output[i] = out4;
-        }
-    }
-    return node;
-})();
+
+
 // source.connect(effect);
 //source.connect(audioCtx.destination);
 
